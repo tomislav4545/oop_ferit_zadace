22bee8b021bab4b3bb28ff8879baa3ece23b1d5f
tomislav4545 <56657176+tomislav4545@users.noreply.github.com>
2020-10-30 20:04:50 +0100

    Add files via upload

---
+++ DZ1/ClassLibrary1/Episode.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography;
+namespace ClassLibrary1
+{
+    public class Episode
+    {
+        private int viewerCount;
+        private double scoreSum;
+        private double maxScore;
+
+        public Episode(int viewerCount, double scoreSum, double maxScore)
+        {
+            this.viewerCount = viewerCount;
+            this.scoreSum = scoreSum;
+            this.maxScore = maxScore;
+        }
+        public Episode() { }
+
+        public int GetViewerCount()
+        {
+            return viewerCount;
+        }
+
+
+        public double GetAverageScore()
+        {
+            return (scoreSum / viewerCount);
+        }
+
+
+        public void AddView(double scoreSum)
+        {
+            this.scoreSum = scoreSum + this.scoreSum;
+
+            if (scoreSum > this.maxScore)
+            {
+                this.maxScore = scoreSum;
+            }
+            this.viewerCount += 1;
+
+        }
+
+
+         public double GenerateRandomScore()
+        {
+            Random rand = new Random();
+            double randScore = rand.NextDouble() * 10;
+            return randScore;
+        }
+
+
+        public double GetMaxScore()
+        {
+            return maxScore;
+        }
+
+    }
+
+}
