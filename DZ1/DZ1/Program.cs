d30440ec3ea7a741029f28c041699a9aed8ac55b
tomislav4545 <56657176+tomislav4545@users.noreply.github.com>
2020-11-14 21:25:16 +0100

    Renamed ClassLibrary to appropriatename

---
+++ DZ1/DZ1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EpisodeNamespace;
+
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Episode ep1, ep2;
+		ep1 = new Episode();
+		ep2 = new Episode(10, 64.39, 8.7);
+		int viewers = 10;
+		for (int i = 0; i < viewers; i++)
+		{
+
+			ep1.AddView(ep1.GenerateRandomScore());
+			Console.WriteLine(ep1.GetMaxScore());
+		}
+		if (ep1.GetAverageScore() > ep2.GetAverageScore())
+		{
+			Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+		}
+		else
+		{
+			Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+		}
+	}
+
+
+
+}
+
