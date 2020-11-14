7993a4def30b8edd10c71b63f2693d0b67d580f2
tomislav4545 <56657176+tomislav4545@users.noreply.github.com>
2020-11-14 21:25:53 +0100

    Update

--- DZ2/Description/Description.cs
+++ DZ2/Description/Description.cs
@@ -4,5 +4,25 @@ namespace Description
 {
     public class Description
     {
+        int episodeNumber;
+        TimeSpan episodeLength;
+        string episodeName;
+        
+        public Description(int episodeNumber,TimeSpan episodeLength, string episodeName)
+        {
+            this.episodeNumber = episodeNumber;
+            this.episodeLength = episodeLength;
+            this.episodeName = episodeName;
+        }
+
+        public Description()
+        {
+            episodeNumber = 0;
+            episodeLength = default(TimeSpan);
+            episodeName = "NULL";
+
+        }
+
+
     }
 }
