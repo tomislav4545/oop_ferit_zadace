441c34b7cd43ea4a053aeceb40bf7bf48ace5dd4
tomislav4545 <tomislav4545@gmail.com>
2020-12-30 00:13:24 +0100

    DZ4 Kod

---
+++ DZ4/DZ4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using EpisodeNamespace;
+using System.Collections.Generic;
+using IPrinterNamespace;
+using TvUtilitiesNamespace;
+using SeasonNamespace;
+using TvExceptionNamespace;
+
+namespace DZ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "shows.tv";
+
+            IPrinter printer = new ConsolePrinter();
+            printer.Print($"Reading data from file {fileName}");
+
+            List<Episode> episodes = TvUtilities.LoadEpisodesFromFile(fileName);
+            Season season = new Season(1, episodes);
+
+            printer.Print(season.ToString());
+            foreach (var episode in season)
+            {
+                episode.AddView(TvUtilities.GenerateRandomScore());
+            }
+            printer.Print(season.ToString());
+
+            Season copy = new Season(season);
+            copy[0].AddView(1.0);
+            if (copy[0].GetAverageScore() == season[0].GetAverageScore())
+            {
+                printer.Print("This is not the correct copy implementation!");
+            }
+
+            try
+            {
+                season.Remove("Pilot");
+                season.Remove("Nope");
+            }
+            catch (TvException e)
+            {
+                printer.Print($"{e.Message}, Name: {e.Title}");
+            }
+            printer.Print(season.ToString());
+        }
+    }
+}
---
+++ DZ4/TvException/TvException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TvExceptionNamespace
+{
+    public class TvException : Exception
+    {
+        public string Title { get; }
+
+        public TvException() { }
+
+        public TvException(string message)
+            : base(message) { }
+
+        public TvException(string message, Exception inner)
+            : base(message, inner) { }
+
+        public TvException(string message, string episodeName)
+            : this(message)
+        {
+            Title = episodeName;
+        }
+
+    }
+}
